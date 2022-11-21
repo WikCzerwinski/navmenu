f541eca8e74a9610d67dc8e6f7e08a323a0b3dcd
Wiktor <112932742+WikCzerwinski@users.noreply.github.com>
2022-11-21 10:43:13 +0100

    some formatting done

--- main.js
+++ main.js
@@ -1,26 +1,26 @@
-
-const output = document.querySelector('.output');
-const output1 = document.createElement('div');
-const ul = document.createElement('ul');
+const output = document.querySelector(".output");
+const output1 = document.createElement("div");
+const ul = document.createElement("ul");
 output.append(output1);
 output.append(ul);
-const url = 'list.json';
-window.addEventListener('DOMContentLoaded',()=>{
-   loadData();
-})
- 
-function loadData(){
-   fetch(url).then(rep=>rep.json())
-   .then((data)=>{
-       addtoPage(data);
-   })
+const url = "list.json";
+window.addEventListener("DOMContentLoaded", () => {
+  loadData();
+});
+
+function loadData() {
+  fetch(url)
+    .then((rep) => rep.json())
+    .then((data) => {
+      addtoPage(data);
+    });
 }
- 
-function addtoPage(arr){
-   arr.forEach((el)=>{
-       console.log(el);
-       const li = document.createElement('li');
-       li.textContent = el.title;
-       ul.append(li);
-   });
+
+function addtoPage(arr) {
+  arr.forEach((el) => {
+    console.log(el);
+    const li = document.createElement("li");
+    li.textContent = el.title;
+    ul.append(li);
+  });
 }
